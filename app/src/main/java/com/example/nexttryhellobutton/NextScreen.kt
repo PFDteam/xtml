4f854391faba2e138277c31f25f6716f4513fe53
PFDteam <151933746+PFDteam@users.noreply.github.com>
2023-12-06 23:30:31 +0300

    ...

---
+++ app/src/main/java/com/example/nexttryhellobutton/NextScreen.kt
@@ -0,0 +1,11 @@
+package com.example.nexttryhellobutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NextScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next_screen)
+    }
+}
